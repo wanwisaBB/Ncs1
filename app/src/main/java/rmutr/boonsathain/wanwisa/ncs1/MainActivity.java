ceba968aa128e98fde454d40dbfae79c40c47156
namfon <wisa_fon77@hotmail.com>
2016-04-14 15:30:51 +0700

    Syn JSON to string

--- app/src/main/java/rmutr/boonsathain/wanwisa/ncs1/MainActivity.java
+++ app/src/main/java/rmutr/boonsathain/wanwisa/ncs1/MainActivity.java
@@ -1,10 +1,16 @@
 package rmutr.boonsathain.wanwisa.ncs1;
 
 import android.database.sqlite.SQLiteDatabase;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,11 +38,51 @@ public class MainActivity extends AppCompatActivity {
         //Delete All SQLite
         deleteAllSQLite();
 
+        //Syn JSON to SQLite
+        synJSON();
+
     }   //Main Method
 
+    private void synJSON() {
+        MyConnectedMain myConnectedMain = new MyConnectedMain();
+        myConnectedMain.execute();
+    }
+
+    //Create Inner Class
+    public class MyConnectedMain extends AsyncTask<Void, Void, String> {
+        @Override
+        protected String doInBackground(Void... params) {
+
+            try {
+
+                OkHttpClient okHttpClient= new OkHttpClient();
+                Request.Builder builder = new Request.Builder();
+                Request request = builder.url("http://swiftcodingthai.com/num/php_get_user_master.php").build();
+                Response response = okHttpClient.newCall(request).execute();
+                return response.body().string();
+
+
+            }catch (Exception e){
+                Log.d("14April", "doIn error ==>" + e.toString());
+                return null;
+            } //try
+
+        }   // doInback
+
+        @Override
+        protected void onPostExecute(String strJSON) {
+            super.onPostExecute(strJSON);
+
+            Log.d("14April", "strJSON ==>" +strJSON);
+
+        }   //onpost
+
+    }   //  MyConnected Class
+
     private void deleteAllSQLite() {
+
         SQLiteDatabase sqLiteDatabase = openOrCreateDatabase(MyOpenHelper.database_name,
-                MODE_PRIVATE,null);
+                MODE_PRIVATE, null);
         sqLiteDatabase.delete(MyManage.user_table, null, null);
     }
 
